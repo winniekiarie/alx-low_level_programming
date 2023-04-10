099cb831039c758d878f8b635f89449fd230cfa1
winniekiarie <kiariebsc@gmail.com>
2023-04-10 06:46:46 -0700

    binary rep of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_binary - Prints the binary representation of a number.
+* @n: The number to be represented in binary.
+*
+* Return: void.
+*/
+
+void print_binary(unsigned long int n)
+{
+int k, count = 0;
+unsigned long int current;
+
+for (k = 63; k >= 0; k--)
+{
+current = n >> k;
+
+if (current & 1)
+{
+_putchar('1');
+count++;
+}
+else if (count)
+{
+_putchar('0');
+}
+}
+
+if (!count)
+{
+_putchar('0');
+}
+}
